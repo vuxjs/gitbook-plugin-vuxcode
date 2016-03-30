89c5c3b5d6b288d1c459f43d4b22aa4fb9b489ee
airyland <airyland@qq.com>
2016-03-30 20:08:39 +0800

    bump 0.0.4

---
+++ src/index.js
@@ -0,0 +1,40 @@
+var parse = require('./libs/parser')
+var co = require('co')
+
+function addTag (code, tag) {
+  var html = `${slash(code)}`
+  return html
+}
+
+function slash (str) {
+  return str.replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/'/g, '&#39;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+}
+
+function encode(str){
+  return encodeURIComponent(str)
+}
+module.exports = {
+  blocks: {
+    vux: {
+      process: function(block) {
+        var data = parse(block.body)
+        var height = block.kwargs.height ? block.kwargs.height : 100
+        var width = block.kwargs.width ? block.kwargs.width : '100%'
+        var url = `https://vux.li/api/v1/demo.html?components=${encode(data.components)}&template=${encode(data.template)}&style=${encode(data.style)}&script=${encode(data.script)}`
+        var html = `<div style=""><iframe src="${url}" frameborder="0" scrolling="no" height="${height}" width="${width}" style="border:none;overflow:hidden;"></iframe></div>`
+        for(var i in data) {
+          if (i==='template' && data[i]) {
+            html +=`<p style="color:#ccc;margin-bottom:0;margin-top:10px;">template</p><pre style="margin:0;padding:0;"><code class="language-html">${addTag(data[i],'template')}</code></pre>`
+          }
+          if (i==='script' && data[i]) {
+            html +=`<p style="color:#ccc;margin-bottom:0;margin-top:10px;">script</p><pre style="margin:0;padding:0;"><code class="language-javascript">${addTag(data[i],'script')}</code></pre>`
+          }
+          if (i==='style' && data[i]) {
+            html +=`<p style="color:#ccc;margin-bottom:0;margin-top:10px;">style</p><pre style="margin:0;padding:0;"><code class="language-css">${addTag(data[i],'style')}</code></pre>`
+          }
+        }
+        return html
+      }
+    }
+  }
+};
