33b5cbe399d18e6c5c9eda8473ad2ad6a99a322b
airyland <airyland@qq.com>
2016-03-30 18:21:03 +0800

    init

---
+++ libs/parser.js
@@ -0,0 +1,21 @@
+var list = ['template', 'script', 'style', 'components']
+var regs = {
+  templateReg: /<template[^>]*>([\s\S]*?)<\/template>/,
+  scriptReg: /<script[^>]*>([\s\S]*?)<\/script>/,
+  styleReg: /<style[^>]*>([\s\S]*?)<\/style>/,
+  componentsReg: /<components[^>]*>([\s\S]*?)<\/components>/
+}
+
+module.exports = function (str) {
+  var data = {}
+  list.forEach(function (one) {
+    var _rs = str.match(regs[one + 'Reg'])
+    data[one] = _rs && _rs[1]? _rs[1]: ''
+    data[one] = data[one].replace(/^\s+|\s+$/g,'')
+  })
+  return data
+}
+
+function removeBreak (str) {
+  return str.replace(/(\r\n|\n|\r)/gm, '')
+}
